b8ff6801bad75d91065018c55976f86c5fa0fd6d
Wagner Hitomi <wagner.hitomi@hubchain.io>
2018-02-25 11:31:37 -0300

    atualizacao install dbb4

---
+++ multi-machine/bootstrap/bdb4.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export BDB_PREFIX="/usr/local/db4"
+
+cd ${BDB_PREFIX}
+
+curl -O http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
+tar -zxf db-4.8.30.NC.tar.gz
+rm -f db-4.8.30.NC.tar.gz
+cd db-4.8.30.NC/build_unix/
+../dist/configure --enable-cxx --disable-shared --with-pic --prefix=$BDB_PREFIX
+make install
