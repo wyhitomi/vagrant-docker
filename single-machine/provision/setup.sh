9082c5391bce968d1dfecb42cb0c96ff2a66e2f8
Wagner Hitomi <wagner.hitomi@hubchain.io>
2018-08-14 13:18:50 -0300

    update to debian stretch

--- single-machine/provision/setup.sh
+++ single-machine/provision/setup.sh
@@ -6,22 +6,28 @@ function prereqs {
       apt-transport-https \
       ca-certificates \
       curl \
+      gnupg2 \
       software-properties-common \
-      git
+      git \
+      git-core \
+      vim \
+      tmux \
+      htop \
+      net-tools
+
 }
 
 function docker {
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
   add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+     "deb [arch=amd64] https://download.docker.com/linux/debian \
+     $(lsb_release -cs) \
+     stable"
    apt-get update \
-   && apt-get install -y docker-ce \
-   && curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose \
-   && curl -L https://github.com/docker/machine/releases/download/v0.12.2/docker-machine-`uname -s`-`uname -m` >/usr/local/bin/docker-machine \
-   && chmod +x /usr/local/bin/docker-* \
-   && usermod -G docker ubuntu
+     && apt-get install -y docker-ce \
+     && curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose \
+     && chmod +x /usr/local/bin/docker-* \
+     && usermod -G docker vagrant
 }
 
 function nodejs {
@@ -34,6 +40,6 @@ function cleanup {
 }
 
 prereqs
-nodejs
+# nodejs
 docker
 cleanup
